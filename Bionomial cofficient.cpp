860bae78e5c0f65c8b636461faf9f71417ec4871
Md. Mahfuj Alam <107198635+mahfujalam77452@users.noreply.github.com>
2026-03-12 13:23:34 +0600

    Update Bionomial cofficient.cpp

--- Bionomial cofficient.cpp
+++ Bionomial cofficient.cpp
@@ -1,47 +1,50 @@
 #include<bits/stdc++.h>
 using namespace std;
-const long long M=1000003;
-long long arr[1000000+5];
 
-long long modInverse(long long A,long long M)
-{
-   long long m0 = M;
-   long long y = 0, x = 1;
- 
-    if (M == 1)
-        return 0;
- 
-    while (A > 1) {
-        long long q = A / M;
-        long long t = M;
-        M = A % M, A = t;
-        t = y;
-        y = x - q * y;
-        x = t;
+#define int long long
+const int MOD = 1e9+7;
+const int MAXN = 200000 + 5;
+
+int fact[MAXN], invfact[MAXN];
+
+int modpow(int a,int b){
+    int ans = 1;
+    
+    while(b){
+        if(b&1) ans = (ans*a)%MOD;
+        
+        a = a*a %MOD;
+        
+        b>>=1;
     }
-    if (x < 0)
-        x += m0;
- 
-    return x;
+    
+    return ans;
 }
- 
+
+void init(){
+    fact[0]=1;
+    for(int i=1;i<MAXN;i++)
+        fact[i]=fact[i-1]*i%MOD;
+
+    invfact[MAXN - 1] = modpow(fact[MAXN - 1],MOD - 2);
+    
+    for(int i = MAXN - 2;i>=0;i--){
+        invfact[i] = (invfact[i+1] * (i + 1))%MOD;
+    }
+}
+
+int nCr(int n,int r){
+    if(r<0||r>n) return 0;
+    return fact[n]*invfact[r]%MOD*invfact[n-r]%MOD;
+}
 int main()
 {
-//This code is to compute nCr with Modulo M;
-    arr[0]=1;
-    for(int i=1;i<=1000003;i++)arr[i]=((i%M)*(arr[i-1]%M))%M;
+
     int t;
     
     cin>>t;
     for(int k=1;k<=t;k++){
-        cout<<"Case "<<k<<": ";
-        long long n,r;
-        cin>>n>>r;
-        long long x=arr[n];
-        long long y=((arr[n-r]%M)*(arr[r]%M))%M;
-        y=modInverse(y,M);
-        long long ans=((x%M)*(y%M))%M;
-        cout<<ans<<endl;
+       
         
     }
     return 0;
